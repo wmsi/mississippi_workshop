6b8aa75d89a8edae9dc908e40903b8c64f3f9522
wmsi <wchurch@whitemountainscience.org>
2018-04-09 21:20:42 +0000

    changed control_led and led_page for new pico

--- remote_led/control_led.py
+++ remote_led/control_led.py
@@ -1,17 +1,43 @@
 import pico 
+from pico import PicoApp
 import RPi.GPIO as GPIO
+import time
 
-# Sets numbering system
-GPIO.setmode(GPIO.BCM)
+red = 18
+yellow = 19
+green = 20
 
-# Set pin 21 for digital output
-led = 21
-GPIO.setup(led, GPIO.OUT)
-
-# Set the initial state to off
-GPIO.output(led, 0)
+def setModeAll():
+	GPIO.setmode(GPIO.BCM)
+	for x in range (18,21):
+		GPIO.setup(x,GPIO.OUT)
 
 # Function called by javascript to set state
-def setState(state):
-	GPIO.output(led, state)
-	print "LED toggled"
+@pico.expose()
+def setStateRed(state=0):
+	setModeAll()
+	GPIO.output(red, state)
+	GPIO.output(yellow,0)
+	GPIO.output(green,0)
+	print "Red LED toggled, yes sir"
+	
+@pico.expose()
+def setStateYellow(state=0):
+	setModeAll()
+	GPIO.output(yellow, state)
+	GPIO.output(red,0)
+	GPIO.output(green,0)
+	print "Yellow LED toggled, yes sir"
+	
+@pico.expose()
+def setStateGreen(state=0):
+	setModeAll()
+	GPIO.output(green, state)
+	GPIO.output(red,0)
+	GPIO.output(yellow,0)
+	print "Green LED toggled, yes sir"
+
+
+
+app=PicoApp()
+app.register_module(__name__)
